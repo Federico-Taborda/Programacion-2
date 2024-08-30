ecf0aa857c7a6a180cf2467a25bfbaf82378073d
Federico-Taborda <panterx.95@gmail.com>
2024-08-29 21:14:55 -0300

    Correciones a funciones recursivas

--- Python - Modulo 1/Ejercicio-1.py
+++ Python - Modulo 1/Ejercicio-1.py
@@ -1,3 +1,11 @@
-for num in range(25):
-    if num % 2 == 0: 
-        print(num)
+def primeros_veinticinco_naturales(contador = 0, m = 0):
+    if contador == 25:
+        return 
+    
+    if m % 2 == 0:
+        print(m)
+        return primeros_veinticinco_naturales(contador + 1, m + 1)
+    
+    return primeros_veinticinco_naturales(contador, m + 1)
+
+print(primeros_veinticinco_naturales())
--- Python - Modulo 1/Ejercicio-2.py
+++ Python - Modulo 1/Ejercicio-2.py
@@ -1,3 +1,11 @@
-for num in range(100):
-    if num % 2 == 0: 
-        print(num)
+def primeros_veinticinco_naturales(contador = 0, m = 0):
+    if contador == 100:
+        return 
+    
+    if m % 2 == 0:
+        print(m)
+        return primeros_veinticinco_naturales(contador + 1, m + 1)
+    
+    return primeros_veinticinco_naturales(contador, m + 1)
+
+print(primeros_veinticinco_naturales())
