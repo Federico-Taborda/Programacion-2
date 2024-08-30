e944154d2e8754e5ef30fc698589f6b760d59c47
Federico-Taborda <panterx.95@gmail.com>
2024-08-29 23:10:13 -0300

    E2 - M3 Ejemplo recursivo agregado

--- Python - Modulo 3/Ejercicio-2.py
+++ Python - Modulo 3/Ejercicio-2.py
@@ -7,4 +7,25 @@ def acumular(lista):
 
     return nueva_lista
 
-print(acumular([1, 2, 3]))
+def acumular_recursivo(lista, i=1, nueva_lista = []):
+    if i == len(lista):
+        return nueva_lista
+    
+    if nueva_lista == []:
+        nueva_lista.append(lista[0])
+    
+    elemento_anterior = nueva_lista[len(nueva_lista) - 1]
+    elemento_actual = lista[i]
+    nueva_lista.append(elemento_anterior + elemento_actual)
+
+    return acumular_recursivo(lista, i + 1, nueva_lista)
+
+print(acumular_recursivo([1,1,1]))
+
+def test_acumular():
+    assert acumular([1,2,3]) == [1,3,6]
+    assert acumular([1,1,1]) == [1,2,3]
+
+def test_acumular_recursivo():
+    pass
+    #assert acumular_recursivo([1,2,3]) == [1,3,6]
