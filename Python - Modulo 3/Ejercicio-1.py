2d7e9a5ea91c6b36fdcf1e858e9343fec279d0e3
Federico-Taborda <panterx.95@gmail.com>
2024-08-29 21:17:21 -0300

    Ejemplo recursivo agregado E1-M3

--- Python - Modulo 3/Ejercicio-1.py
+++ Python - Modulo 3/Ejercicio-1.py
@@ -12,3 +12,18 @@ print(posiciones_multiplo([1,2,3,4,5,6,7],2))
 def test_posiciones_multiplo():
     assert posiciones_multiplo([1,2,3,4,5,6,7],2) == [1, 3, 5]
     assert posiciones_multiplo([1,2,3,4,5,6,7],3) == [2, 5]
+
+def posiciones_multiplo_recursiva(lista, n, i=0, nueva_lista = []):
+    if i == len(lista):
+        return nueva_lista
+    
+    if lista[i] % n == 0:
+        nueva_lista.append(i)
+
+    return posiciones_multiplo_recursiva(lista, n, i + 1, nueva_lista)
+
+print(posiciones_multiplo_recursiva([1,2,3,4,5,6,7],2))
+
+def test_posiciones_multiplo_recursiva():
+    assert posiciones_multiplo_recursiva([1,2,3,4,5,6,7],2) == [1, 3, 5]
+    assert posiciones_multiplo_recursiva([1,2,3,4,5,6,7],3) == [2, 5]
