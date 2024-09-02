9362426be30a40b97ea341350b04922fe6980df2
Federico-Taborda <panterx.95@gmail.com>
2024-09-02 19:17:33 -0300

    E13, E15 - M3 agregados

---
+++ Python - Modulo 3/Ejercicio-13.py
@@ -0,0 +1,43 @@
+import random
+
+cartas = [ "picas", "trebol", "diamantes", "corazones" ]
+
+def tomar_carta():
+    numero = random.randint(1, 13)
+    carta = random.randint(0, 3)
+
+    carta_tomada = cartas[carta]
+
+    if numero == 1: return "As ", carta_tomada
+    if numero == 11: return "J", carta_tomada
+    if numero == 12: return "Q", carta_tomada
+    if numero == 13: return "K", carta_tomada
+    
+    return numero, carta_tomada
+
+def contar_repeticiones(mano):
+
+    for carta in mano:
+        repeticiones = 0    
+        for otra_carta in mano:
+            if carta[0] == otra_carta[0]:
+                repeticiones += 1
+        
+        if repeticiones >= 4:
+            print(mano)
+            return True
+    
+    return False
+
+def poker():
+    mano = []
+
+    while len(mano) < 5:
+        carta = tomar_carta()
+        if carta not in mano:
+            mano.append(carta)
+    
+    return contar_repeticiones(mano)
+
+for x in range(10000):
+    poker()
---
+++ Python - Modulo 3/Ejercicio-15.py
@@ -0,0 +1,10 @@
+def diaSiguiente(fecha):
+    if fecha[0] == 30 and fecha[1] == 12:
+        return (1, 1, fecha[2] + 1)
+    
+    if fecha[0] == 30:
+        return (1, fecha[1] + 1, fecha[2])
+    
+    return (fecha[0] + 1, fecha[1], fecha[2])
+
+print(diaSiguiente((30, 12, 2000)))
