7f858d0bebe3f8673b9ef30107c9d202cfaa9a4f
Federico-Taborda <panterx.95@gmail.com>
2024-09-15 19:34:27 -0300

    E10 - M3 casos iterativos, caso recursivo, tests agregados

--- Python - Modulo 3/Ejercicio-10.py
+++ Python - Modulo 3/Ejercicio-10.py
@@ -1,4 +1,4 @@
-def contar(l, cadena):
+def contar_for(l, cadena):
     contador = 0
 
     for x in cadena:
@@ -7,4 +7,33 @@ def contar(l, cadena):
         
     return contador
 
-print(contar("a", "palabra"))
+def contar_while(l, palabra):
+    indice = 0
+    contador = 0
+
+    while indice < len(palabra):
+        if palabra[indice] == l:
+            contador += 1
+        indice += 1
+    
+    return contador
+
+def contar_recursiva(l, palabra, indice = 0, contador = 0):
+    if indice == len(palabra):
+        return contador
+    
+    if palabra[indice] == l:
+        return contar_recursiva(l, palabra, indice + 1, contador + 1)
+    
+    return contar_recursiva(l, palabra, indice + 1, contador)
+
+def test_contar():
+    assert contar_for("a", "") == 0
+    assert contar_for("e", "palabra") == 0
+    assert contar_for("a", "palabra") == 3
+    assert contar_while("a", "") == 0
+    assert contar_while("e", "palabra") == 0
+    assert contar_while("a", "palabra") == 3
+    assert contar_recursiva("a", "") == 0
+    assert contar_recursiva("e", "palabra") == 0
+    assert contar_recursiva("a", "palabra") == 3
