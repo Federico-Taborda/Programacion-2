c0b290520c0ad2d980e101135a6b42aaf2d00059
Federico-Taborda <panterx.95@gmail.com>
2024-09-15 17:19:49 -0300

    E3 - M3 diferentes formas agregadas

--- Python - Modulo 3/Ejercicio-3.py
+++ Python - Modulo 3/Ejercicio-3.py
@@ -1,12 +1,30 @@
 def elimina_uno(lista):
     nueva_lista = []
-    for i in range(1, len(lista) - 1):
+    for i in range(1, len(lista)):
         nueva_lista.append(lista[i])
     
     return nueva_lista
 
 def elimina_dos(lista): 
-    return lista[1:len(lista) - 1]
+    return lista[1:len(lista)]
 
-print(elimina_uno(["a", "b", "c", "d"]))
-print(elimina_dos(["a", "b", "c", "d"]))
+# Esta opcion elimina el primer elemento de la lista fuera del ambito de la funcion
+def elimina_tres(lista):
+    del lista[0]
+    return lista
+
+# Esta opcion elimina el primer elemento de la lista dentro del ambito de la funcion
+def elimina_cuatro(lista):
+    nueva_lista = []
+    nueva_lista += lista
+
+    del nueva_lista[0]
+    return nueva_lista
+
+arr = ["a", "b", "c", "d"]
+print(elimina_uno(arr))
+print(elimina_dos(arr))
+#print(elimina_tres(arr))
+#print(elimina_cuatro(arr))
+
+print(arr)
