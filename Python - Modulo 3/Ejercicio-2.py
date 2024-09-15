041db876ba7bc7a27b76875def1be3ad983287d5
Federico-Taborda <panterx.95@gmail.com>
2024-09-15 17:19:14 -0300

    E2 - M2 caso iterativo agregado

--- Python - Modulo 3/Ejercicio-2.py
+++ Python - Modulo 3/Ejercicio-2.py
@@ -1,9 +1,19 @@
-def acumular(lista):
+def acumular_for(lista):
     nueva_lista = [lista[0]]
     
     for i in range(1, len(lista)):
         elemento_anterior =  nueva_lista[len(nueva_lista) - 1]
         nueva_lista.append(lista[i] + elemento_anterior)
+
+    return nueva_lista
+
+def acumular_while(lista):
+    nueva_lista = [lista[0]]
+    indice = 1
+
+    while indice < len(lista):
+        nueva_lista.append(nueva_lista[indice - 1] + lista[indice])
+        indice += 1
 
     return nueva_lista
 
@@ -20,12 +30,13 @@ def acumular_recursivo(lista, i=1, nueva_lista = []):
 
     return acumular_recursivo(lista, i + 1, nueva_lista)
 
-print(acumular_recursivo([1,1,1]))
+print(acumular_while([1,2,3]))
 
 def test_acumular():
-    assert acumular([1,2,3]) == [1,3,6]
-    assert acumular([1,1,1]) == [1,2,3]
+    assert acumular_for([1,2,3]) == [1,3,6]
+    assert acumular_for([1,1,1]) == [1,2,3]
+    assert acumular_while([1,2,3]) == [1,3,6]
+    assert acumular_while([1,1,1]) == [1,2,3]
 
 def test_acumular_recursivo():
-    pass
-    #assert acumular_recursivo([1,2,3]) == [1,3,6]
+    assert acumular_recursivo([1,2,3]) == [1,3,6]
