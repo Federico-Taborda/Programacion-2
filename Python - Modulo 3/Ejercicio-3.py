fa7b1aad78f61b19f45c7b8cd216970344656825
Federico-Taborda <panterx.95@gmail.com>
2024-09-17 19:56:22 -0300

    E3 - M3 corregido y tests agregados

--- Python - Modulo 3/Ejercicio-3.py
+++ Python - Modulo 3/Ejercicio-3.py
@@ -1,16 +1,17 @@
 def elimina_uno(lista):
     nueva_lista = []
-    for i in range(1, len(lista)):
+    for i in range(1, len(lista) - 1):
         nueva_lista.append(lista[i])
     
     return nueva_lista
 
 def elimina_dos(lista): 
-    return lista[1:len(lista)]
+    return lista[1:len(lista) - 1]
 
 # Esta opcion elimina el primer elemento de la lista fuera del ambito de la funcion
 def elimina_tres(lista):
     del lista[0]
+    del lista[len(lista) - 1]
     return lista
 
 # Esta opcion elimina el primer elemento de la lista dentro del ambito de la funcion
@@ -19,12 +20,13 @@ def elimina_cuatro(lista):
     nueva_lista += lista
 
     del nueva_lista[0]
+    del nueva_lista[len(nueva_lista) - 1]
     return nueva_lista
 
-arr = ["a", "b", "c", "d"]
-print(elimina_uno(arr))
-print(elimina_dos(arr))
-#print(elimina_tres(arr))
-#print(elimina_cuatro(arr))
 
-print(arr)
+def test_elimina():
+    assert elimina_uno([1,2,3]) == [2]  
+    assert elimina_dos([1,2,3]) == [2]
+    assert elimina_tres([1,2,3]) == [2]
+    assert elimina_cuatro([1,2,3]) == [2]
+
