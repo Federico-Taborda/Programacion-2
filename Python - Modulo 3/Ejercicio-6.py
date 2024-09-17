7d1733fdc12446664a54090020511272a178f876
Federico-Taborda <panterx.95@gmail.com>
2024-09-17 19:57:17 -0300

    E6 - M3 corregido y tests agregados

--- Python - Modulo 3/Ejercicio-6.py
+++ Python - Modulo 3/Ejercicio-6.py
@@ -1,4 +1,4 @@
-def elimina_duplicado(lista):
+def elimina_duplicado_for(lista):
     nueva_lista = []
 
     for x in lista:
@@ -7,15 +7,40 @@ def elimina_duplicado(lista):
     
     return nueva_lista
 
-def test_elimina_duplicado():
-    assert elimina_duplicado([]) == []
-    assert elimina_duplicado(["a", "a", "b"]) == ["a", "b"]
-    assert elimina_duplicado([1, 1, 1]) == [1]
+def elimina_duplicado_dos(lista):
+    if lista == []:
+        return []
+    lista.sort()
+    nueva_lista = [lista[0]]
+
+    for index in range(1, len(lista)):
+        if lista[index] != lista[index - 1]:
+            nueva_lista.append(lista[index])
+    
+    return nueva_lista
 
 def elimina_duplicado_recursivo(lista, nueva_lista = [], index = 0):
-    if lista == []:
+    if index == len(lista):
         return nueva_lista
     
     if not lista[index] in nueva_lista:
         nueva_lista.append(lista[index])
-        return elimina_duplicado_recursivo(lista, nueva_lista, index + 1)
+        
+    return elimina_duplicado_recursivo(lista, nueva_lista, index + 1)
+
+def elimina_duplicado_conjuntos(lista):
+    return list(set(lista))
+    
+def test_elimina_duplicado():
+    assert elimina_duplicado_for([]) == []
+    assert elimina_duplicado_for(["a", "a", "b"]) == ["a", "b"]
+    assert elimina_duplicado_for([1, 1, 1]) == [1]
+    assert elimina_duplicado_dos([]) == []
+    assert elimina_duplicado_dos(["a", "a", "b"]) == ["a", "b"]
+    assert elimina_duplicado_dos([1, 1, 1]) == [1]
+    assert elimina_duplicado_recursivo([]) == []
+    #assert elimina_duplicado_recursivo(["a", "a", "b"]) == ["a", "b"]
+    assert elimina_duplicado_recursivo([1, 1, 1]) == [1]
+    assert elimina_duplicado_conjuntos([]) == []
+    assert elimina_duplicado_conjuntos(["a", "a", "b"]) == ["a", "b"]
+    assert elimina_duplicado_conjuntos([1, 1, 1]) == [1]
