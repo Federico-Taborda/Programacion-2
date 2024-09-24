6bf96f6a2cecf3cfdb07cac1650f0016d3f1b03e
Federico-Taborda <panterx.95@gmail.com>
2024-09-24 20:26:02 -0300

    E3- M4 corregido

--- Python - Modulo 4/Ejercicio-3.py
+++ Python - Modulo 4/Ejercicio-3.py
@@ -1,35 +1,40 @@
-from math import *
-
 def busqueda_binaria(lista, elemento):
-    low = 0
-    mid = ceil(len(lista) / 2)
-    high = len(lista) - 1
-    found = False
+    primero = 0
+    ultimo = len(lista) - 1
+    mitad = len(lista) // 2
+    encontrado = False
     index = 0
     
-    while not found:
-        if lista[low] == elemento:
-            found = True
-            index = low
+    while not encontrado:
+        if lista[primero] == elemento:
+            encontrado = True
+            index = primero
         
-        if lista[high] == elemento:
-            found = True
-            index = high
+        if lista[ultimo] == elemento:
+            encontrado = True
+            index = ultimo
         
-        if lista[mid] == elemento:
-            found = True
-            index = mid
+        if lista[mitad] == elemento:
+            encontrado = True
+            index = mitad
+            
+        if lista[mitad] > elemento:
+            ultimo = mitad
+            mitad = ultimo // 2
+        
+        if lista[mitad] < elemento:
+            primero = mitad + 1
+            mitad = mitad + (ultimo - mitad) // 2
 
-        if lista[mid] > elemento:
-            high = mid
-            mid = high // 2
-        else:
-            low = mid + 1
-            mid = mid + ((high - mid) // 2)
-     
+        if lista[mitad] != elemento:
+            print(lista[:mitad])
+            encontrado = True
+
     return index
+print(busqueda_binaria(["a", "c", "d", "e", "f"], "b"))
 
 def test_busqueda_binaria():
     assert busqueda_binaria([1,2,3,4,5,6,7,8,9,10], 4) == 3
-    assert busqueda_binaria(["a", "b", "c", "d", "e", "f"], "b") == 2
+    assert busqueda_binaria(["a", "b", "c", "d", "e", "f"], "b") == 1
+    assert busqueda_binaria(["a", "c", "d", "e", "f"], "b") == 1
     
