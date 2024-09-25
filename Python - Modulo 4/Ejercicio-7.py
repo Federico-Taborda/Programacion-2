c02d690fac51fc7e4896180c977dd4eafb996356
Federico-Taborda <panterx.95@gmail.com>
2024-09-24 22:03:28 -0300

    E7 - M4 agregado

---
+++ Python - Modulo 4/Ejercicio-7.py
@@ -0,0 +1,22 @@
+notas = {
+    "Federico": [1, 1, 6],
+    "Gabriel": [7, 7, 8],
+    "Ezequiel": [8, 9, 8]
+}
+
+def promedio(notas):
+    diccionario = {}
+
+    for alumno in notas:
+        total = 0
+        for nota in notas[alumno]:
+            total += nota
+        
+        diccionario[alumno] = total // len(notas[alumno])
+
+    return diccionario
+
+print(promedio(notas))
+
+
+
