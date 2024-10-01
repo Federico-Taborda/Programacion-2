c4884fe8a244efb0f6f104d43e10214a1a2163bb
Federico-Taborda <panterx.95@gmail.com>
2024-09-30 23:35:22 -0300

    Python - E5 - M5 agregado

---
+++ Python - Modulo 5/Ejercicio-5.py
@@ -0,0 +1,36 @@
+def rot13(archivo_origen, archivo_destino):
+    file = open(archivo_origen, "r")
+
+    for linea in file:
+        linea_cifrada = cifrar_linea(linea)
+        guardar_linea_cifrada(linea_cifrada, archivo_destino)
+    
+    file.close
+
+def cifrar_linea(linea):
+    alfabeto = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+    linea_cifrada = ""
+
+    for caracter in range(0, len(linea)):
+        if linea[caracter] != " " and linea[caracter] != "\n" :        
+            indice = alfabeto.index(linea[caracter].lower())
+
+            if indice + 13 > (len(alfabeto) - 1):
+                indice = (indice + 13) - len(alfabeto)
+            else:
+                indice += 13
+            
+            linea_cifrada += alfabeto[indice]
+        
+        if linea[caracter] == " ":
+            linea_cifrada += " "
+
+    return linea_cifrada + "\n"
+
+def guardar_linea_cifrada(linea, archivo):
+    file = open(archivo, "a")
+    file.write(linea)
+    file.close
+
+rot13("oraciones.txt", "oraciones-cifradas.txt")
+rot13("oraciones-cifradas.txt", "oraciones-desifradas.txt")
