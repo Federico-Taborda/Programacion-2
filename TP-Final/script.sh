0e12f7c74ee695cda590e74736f2fe774519ddab
Federico-Taborda <panterx.95@gmail.com>
2024-12-08 00:16:17 -0300

    Script de python y tests

--- TP-Final/script.sh
+++ TP-Final/script.sh
@@ -1,4 +1,10 @@
 #!/bin/zsh
 
 # Script de pruebas para el archivo main.c
-gcc -Wall -Wextra -Werror ./main.c -lm && valgrind ./a.out
+#gcc -Wall -Wextra -Werror ./main.c -lm && valgrind ./a.out
+
+# Script de pruebas para el archivo main.py
+#gcc -Wall -Wextra -Werror ./main.c -lm && python3 main.py
+
+# Script para correr los test de python
+pytest test_main.py
